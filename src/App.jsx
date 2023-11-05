b27fa637b4118a582a983c004673db0189010aab
Muhammad Haris <iharisimran@gmail.com>
2023-11-05 17:42:23 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   const data = "This is the secret data that only I and the Government of United States know! 🤫",
     secretKey = "@Qwerty123",
     encrypted = CryptoJS.AES.encrypt(data, secretKey),
-    decrypted = CryptoJS.AES.decrypt("U2FsdGVkX1/S1230jC8MXGohDK4aFeOUpt7BDppGyAo2C0S8GUQPlVWCczdxDYxGokcfJpb7tRMhkSMekfubvBGCly4LetZ7fQIYGW+bpm8LCDxJXGkPXWOySYq0X62f", secretKey),
+    decrypted = CryptoJS.AES.decrypt(encrypted, secretKey),
     formatted = decrypted.toString(CryptoJS.enc.Utf8);
 
   console.log(`Origional: ${data}`);
